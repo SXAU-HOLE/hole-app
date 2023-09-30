5e360870da208e535dbe41a51e49218bf12d0cf3
彬腾 <2393714045@qq.com>
2023-09-30 12:04:49 +0800

    feat: 集成eslint格式化代码

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
- import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='font-semibold text-2xl'>123123123123</Text>
+      <Text className="font-semibold text-2xl">123123123123</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
