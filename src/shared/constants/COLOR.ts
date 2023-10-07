9df455c0ebf742c05363f965bd0dc919601e1370
meowrain <meowrain@126.com>
2023-10-07 12:59:12 +0800

    chore: Remove unused components

--- src/shared/constants/COLOR.ts
+++
@@ -1,6 +0,0 @@
-// COLOR.ts
-
-export const PRIMARY_COLOR = 'rgb(38, 149, 83)';
-export const SECONDARY_COLOR = 'rgb(187, 213, 190)';
-export const BACKGROUND_COLOR = '#FFFFFF';
-export const TEXT_COLOR = 'rgb(24, 112, 31)'
