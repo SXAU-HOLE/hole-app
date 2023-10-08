503fd72957f133824052a7da7e5a63bf87fed53e
彬腾 <2393714045@qq.com>
2023-10-08 14:37:50 +0800

    feat: 引入toast-message组件

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,11 +1,13 @@
 import { View } from 'react-native'
 import React from 'react'
 import { Routes } from '@/router/routes'
+import Toast from 'react-native-toast-message'
 
 const layout = () => {
   return (
     <View className={'min-h-[100vh] w-screen'}>
       <Routes></Routes>
+      <Toast></Toast>
     </View>
   )
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,13 @@
 import { RequestConfig } from '@/shared/config'
 import axios, { AxiosError, AxiosRequestConfig } from 'axios'
+import Toast from 'react-native-toast-message'
+
+export interface ErrorResponse {
+  code: number
+  message: string
+  error: string
+  time: string
+}
 
 const instance = axios.create({
   baseURL: RequestConfig.baseUrl,
@@ -7,10 +15,24 @@ const instance = axios.create({
 
 instance.interceptors.response.use(
   (data) => {
-    console.log(data)
-    return data
+    return data.data
   },
   (error: AxiosError) => {
+    const data = error.response?.data as ErrorResponse
+    if (data) {
+      const msg = data.message
+
+      Toast.show({
+        type: 'error',
+        text1: '请求失败了',
+        text2: !msg
+          ? '可能是服务器出问题啦，请联系管理员'
+          : Array.isArray(msg)
+          ? msg.map((i) => i).join('\n')
+          : msg,
+      })
+    }
+
     throw error
   },
 )
