d6daccf77d09368a0e48754a7ce3f30c4ff348e6
彬腾 <2393714045@qq.com>
2023-10-16 22:21:03 +0800

    feat: 头部样式

---
+++ src/pages/hole/HoleHot.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+const HoleHot = () => {
+    return (
+        <View>
+             <Text>HoleHot</Text>
+        </View>
+    );
+};
+
+export default HoleHot;
---
+++ src/pages/hole/components/Header.tsx
@@ -0,0 +1,21 @@
+import {Appbar, Text} from "react-native-paper";
+import {View} from "react-native";
+import {useNavigation} from "@react-navigation/native";
+import {useEffect} from "react";
+
+export function HoleHeader() {
+    const navigator = useNavigation()
+    const currentRouteName = navigator.getState().routeNames[0]
+
+    return (
+        <Appbar.Header className={'bg-transparent fixed flex justify-between'}>
+            <View className={'flex flex-row items-center'}>
+                <Text variant={"headlineMedium"} className={'text-primary'} onPress={() => navigator.navigate('latest')}>时间轴</Text>
+                <Text className={'text-2xl text-primary mx-2'}>/</Text>
+                <Text variant={'headlineSmall'} className={'text-surface'} onPress={() => navigator.navigate('hot')}>随机漫步</Text>
+            </View>
+            <Appbar.Action icon="magnify" onPress={() => {
+            }}/>
+        </Appbar.Header>
+    )
+}
