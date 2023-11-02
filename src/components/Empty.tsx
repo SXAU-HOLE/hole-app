041f58044ae670c8d2064a9fa675408d87585fdc
彬腾 <2393714045@qq.com>
2023-11-02 13:27:50 +0800

    feat: 列表空状态

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,3 +1,21 @@
-export function Empty() {
-  return <></>
+import { Image, ImageProps, View } from 'react-native'
+import { SecondaryText } from '@/components/Text/SecondaryText'
+
+interface Props extends Partial<ImageProps> {
+  text?: string
+}
+
+export function Empty({ text }: Props) {
+  return (
+    <View className={'w-full flex items-center justify-center'}>
+      <Image
+        source={require('@/assets/imgs/empty.png')}
+        style={{
+          width: 200,
+          height: 200,
+        }}
+      />
+      <SecondaryText>{text ?? '没有更多数据了哦'}</SecondaryText>
+    </View>
+  )
 }
